fc5f607df651a4ef0a5ea21a99dcf5bb0e1539c5
Angel Tankov <angel@accelerise.com>
2020-07-22 19:35:15 +0300

    First commit

---
+++ DatingApp.API/Data/DataContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace DatingApp.API.Data
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        DataContext IDesignTimeDbContextFactory<DataContext>.CreateDbContext(string[] args)
+        {
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../DatingApp.API"))
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
