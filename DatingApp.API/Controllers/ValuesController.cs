ffc364524df080b344dd184043de48ae115f64ac
Angel Tankov <angel@accelerise.com>
2020-07-26 22:08:25 +0300

    Add JWT authentication

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -18,7 +20,6 @@ namespace DatingApp.API.Controllers
             this._context = context;
         }
         
-        // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -26,7 +27,7 @@ namespace DatingApp.API.Controllers
             return Ok(vals);
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
